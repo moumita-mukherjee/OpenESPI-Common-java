e960ede53ec83052df467b681da72fd45273c940
mukerjem <mukerjem@W06334055.londonhydro.com>
2016-09-22 11:16:34 -0400

    resolve merge conflict

--- src/main/java/org/energyos/espi/common/repositories/jpa/NotificationQueueRepositoryImpl.java
+++ src/main/java/org/energyos/espi/common/repositories/jpa/NotificationQueueRepositoryImpl.java
@@ -6,140 +6,128 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+
+
+
+
+
 import org.energyos.espi.common.domain.NotificationQueue;
 import org.energyos.espi.common.repositories.NotificationQueueRepository;
 import org.springframework.transaction.annotation.Transactional;
 
-public class NotificationQueueRepositoryImpl implements	NotificationQueueRepository {
-
-	@PersistenceContext(unitName = "pu-energy")
-	protected EntityManager em;
-
+public class NotificationQueueRepositoryImpl implements
+		NotificationQueueRepository {
+	
+	@PersistenceContext(unitName = "pu-energy")	
+    protected EntityManager em;
+	
 	@Override
 	public List<NotificationQueue> findByApplicationInformationId(
 			Long applicationInformationId) {
-		return (List<NotificationQueue>) em
-				.createNamedQuery(
-						NotificationQueue.QUERY_FIND_BY_APPLICATION_INFORMATION_ID)
-				.setParameter("applicationInformationId",
-						applicationInformationId).getResultList();
+		return (List<NotificationQueue>)em.createNamedQuery(NotificationQueue.QUERY_FIND_BY_APPLICATION_INFORMATION_ID)
+                .setParameter("applicationInformationId", applicationInformationId).getResultList();
 	}
 
 	@Override
 	public List<NotificationQueue> findAll() {
-		return (List<NotificationQueue>) em.createNamedQuery(
-				NotificationQueue.QUERY_FIND_ALL).getResultList();
-
+		return (List<NotificationQueue>)em.createNamedQuery(NotificationQueue.QUERY_FIND_ALL).getResultList();
+                
 	}
 
 	@Override
 	public List<NotificationQueue> findByStatus(int status) {
-		return (List<NotificationQueue>) em
-				.createNamedQuery(
-						NotificationQueue.QUERY_FIND_BY_APPLICATION_INFORMATION_ID)
-				.setParameter("status", status).getResultList();
+		return (List<NotificationQueue>)em.createNamedQuery(NotificationQueue.QUERY_FIND_BY_APPLICATION_INFORMATION_ID)
+                .setParameter("status", status).getResultList();
 	}
 
 	@Override
 	public NotificationQueue findById(Long id) {
-		return (NotificationQueue) em
-				.createNamedQuery(NotificationQueue.QUERY_FIND_BY_ID)
-				.setParameter("id", id).getSingleResult();
+		return (NotificationQueue)em.createNamedQuery(NotificationQueue.QUERY_FIND_BY_ID)
+                .setParameter("id", id).getSingleResult();
 	}
-
-	@Transactional
+@Transactional
 	@Override
 	public void createNotificationQueue(NotificationQueue notificationQueue) {
 		em.joinTransaction();
 		em.persist(notificationQueue);
 		em.flush();
-
+		
 	}
-
 	@Transactional
 	@Override
 	public void mergeNotificationQueue(NotificationQueue notificationQueue) {
-		NotificationQueue existingNotificationQueue = null;
-		if (notificationQueue != null)
+		NotificationQueue existingNotificationQueue=null;
+		if(notificationQueue!=null)
 			existingNotificationQueue = findById(notificationQueue.getId());
-		System.err.println(":::notification:::"
-				+ notificationQueue.getResourceUri());
-		if (existingNotificationQueue != null)
-			updateExistingNotificationQueue(existingNotificationQueue,
-					notificationQueue);
+		System.err.println(":::notification:::"+notificationQueue.getResourceUri());
+		if(existingNotificationQueue!=null)
+			updateExistingNotificationQueue(existingNotificationQueue, notificationQueue);
 		em.joinTransaction();
 		em.merge(existingNotificationQueue);
 		em.flush();
-
+		
 	}
-
-	private void updateExistingNotificationQueue(
-			NotificationQueue existingNotificationQueue,
-			NotificationQueue notificationQueue) {
-
+	private void updateExistingNotificationQueue(NotificationQueue existingNotificationQueue,NotificationQueue notificationQueue){
+				
 		existingNotificationQueue.setStatus(notificationQueue.getStatus());
-		existingNotificationQueue.setResourceUri(notificationQueue
-				.getResourceUri());
+		existingNotificationQueue.setResourceUri(notificationQueue.getResourceUri());
+		//Other updatable fields
+		
+	}
+	
+	@Override
+	public void updateNotificationQueueStatusByAppInfoId(List<NotificationQueue> notifications ){
+		for(NotificationQueue notify : notifications){
+				try {
+					em.merge(notify);
+					em.flush();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					System.err.println(" :::: update error is :::: " + e.getCause());
+					System.err.println(" :::: update error is :::: " + e.getMessage());
+				}
+		}
+		
 	}
 
 	@Override
-	public void updateNotificationQueueStatusByAppInfoId(
-			List<NotificationQueue> notifications) {
-		for (NotificationQueue notify : notifications) {
-			try {
-				em.merge(notify);
-				em.flush();
-			} catch (Exception e) {
-
-				System.err
-						.println(" :::: update error is :::: " + e.getCause());
-				System.err.println(" :::: update error is :::: "
-						+ e.getMessage());
-			}
+	public List<NotificationQueue> findByStatusAndApplicationId(int status,Long applicationInformationId) {
+		// TODO Auto-generated method stub
+		 List<NotificationQueue> returnList = new ArrayList<NotificationQueue>();
+		 
+		System.err.println(" :::: Application ID :::: "+applicationInformationId +" :::: Status :::: "+status);
+		 try{
+			returnList= (List<NotificationQueue>)em.createNamedQuery(NotificationQueue.QUERY_FIND_BY_STATUS_AND_APPLICATION_INFORMATION_ID)
+            .setParameter("status", status)
+            .setParameter("applicationInformationId", applicationInformationId)
+            .getResultList();
+		}catch(Exception ex){
+			System.err.println(" :::: query error :::: "+ex.getCause());
+			System.err.println(" :::: query error :::: "+ex.getMessage());
 		}
-
-	}
-
-	@Override
-	public List<NotificationQueue> findByStatusAndApplicationId(int status,
-			Long applicationInformationId) {
-		List<NotificationQueue> returnList = new ArrayList<NotificationQueue>();
-
-		System.err.println(" :::: Application ID :::: "
-				+ applicationInformationId + " :::: Status :::: " + status);
-		try {
-			returnList = (List<NotificationQueue>) em
-					.createNamedQuery(
-							NotificationQueue.QUERY_FIND_BY_STATUS_AND_APPLICATION_INFORMATION_ID)
-					.setParameter("status", status)
-					.setParameter("applicationInformationId",
-							applicationInformationId).getResultList();
-		} catch (Exception ex) {
-			System.err.println(" :::: query error :::: " + ex.getCause());
-			System.err.println(" :::: query error :::: " + ex.getMessage());
-		}
-
+		
 		return returnList;
-
+		
 	}
-
+	
 	@Override
 	@Transactional
 	public void updateByApplicationId(Long applicationInformationId) {
-
-		System.err.println(" :::: Application ID :::: "
-				+ applicationInformationId);
-		try {
+		// TODO Auto-generated method stub
+				 
+		System.err.println(" :::: Application ID :::: "+applicationInformationId);
+		 try{
 			em.joinTransaction();
-			em.createNamedQuery(
-					NotificationQueue.QUERY_UPDATE_BY_APPLICATION_INFORMATION_ID)
-					.setParameter("applicationInformationId",
-							applicationInformationId).executeUpdate();
-		} catch (Exception ex) {
-			System.err.println(" :::: query error :::: " + ex.getCause());
-			System.err.println(" :::: query error :::: " + ex.getMessage());
+			em.createNamedQuery(NotificationQueue.QUERY_UPDATE_BY_APPLICATION_INFORMATION_ID)
+            .setParameter("applicationInformationId", applicationInformationId)
+            .executeUpdate();
+		}catch(Exception ex){
+			System.err.println(" :::: query error :::: "+ex.getCause());
+			System.err.println(" :::: query error :::: "+ex.getMessage());
 		}
-
+		
+		//return returnList;
+		
 	}
 
 }
